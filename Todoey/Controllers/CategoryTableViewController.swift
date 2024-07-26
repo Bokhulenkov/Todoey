50d61c78c699e4cd0b1ea5b30fc81517214d44f6
Bokhulenkov <bokhulenkov@gmail.com>
2024-07-26 10:43:57 +0300

    refactoring categories

--- Todoey/Controllers/CategoryTableViewController.swift
+++ Todoey/Controllers/CategoryTableViewController.swift
@@ -14,7 +14,7 @@ class CategoryTableViewController: UITableViewController {
 //    объявление и создание Realm
     let realm = try! Realm()
     
-    var categories: Results<Category>!
+    var categories: Results<Category>?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -76,12 +76,13 @@ class CategoryTableViewController: UITableViewController {
 extension CategoryTableViewController {
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return categories.count
+        return categories?.count ?? 1
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
-        cell.textLabel?.text = categories[indexPath.row].name
+        
+        cell.textLabel?.text = categories?[indexPath.row].name ?? "No categories"
         cell.selectionStyle = .none
         
         return cell
@@ -95,7 +96,7 @@ extension CategoryTableViewController {
         let destinationVC = segue.destination as! TodoListViewController
         
         if let indexPath = tableView.indexPathForSelectedRow {
-            destinationVC.selectedCategory = categories[indexPath.row]
+            destinationVC.selectedCategory = categories?[indexPath.row]
         }
         
         
